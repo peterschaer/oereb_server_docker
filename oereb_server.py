d5d80d168488e182cdfc3c2d79cd9284cd87b46f
Peter Schaer <peter.schaer@be.ch>
2020-12-04 15:18:52 +0100

    main-Funktion in oereb_server.py eingebaut.

--- oereb_server.py
+++ oereb_server.py
@@ -5,20 +5,24 @@ from mako.template import Template
 import codecs
 import os
 
-# Alle Umgebungsvariablen in einen Dictionary abfüllen
-env_vars = ['POSTGRES_SERVICE_DATABASE', 'POSTGRES_SERVICE_HOST', 'POSTGRES_SERVICE_PASS', 'POSTGRES_SERVICE_PORT', 'POSTGRES_SERVICE_USER', 'PRINT_SERVICE_HOST', 'PRINT_SERVICE_PORT']
-vars = {}
-for env_var in env_vars:
-    vars[env_var] = os.environ[env_var]
+def main():
+    # Alle Umgebungsvariablen in einen Dictionary abfüllen
+    env_vars = ['POSTGRES_SERVICE_DATABASE', 'POSTGRES_SERVICE_HOST', 'POSTGRES_SERVICE_PASS', 'POSTGRES_SERVICE_PORT', 'POSTGRES_SERVICE_USER', 'PRINT_SERVICE_HOST', 'PRINT_SERVICE_PORT']
+    vars = {}
+    for env_var in env_vars:
+        vars[env_var] = os.environ[env_var]
 
-# pyramid_oereb_standard.yml schreiben
-template = Template(filename='pyramid_oereb_standard.mako')
-rendered_config = template.render(**vars)
+    # pyramid_oereb_standard.yml schreiben
+    template = Template(filename='pyramid_oereb_standard.mako')
+    rendered_config = template.render(**vars)
 
-with codecs.open('pyramid_oereb_standard.yml', "w", "utf-8") as config_file:
-    config_file.write(rendered_config)
+    with codecs.open('pyramid_oereb_standard.yml', "w", "utf-8") as config_file:
+        config_file.write(rendered_config)
 
-ini_path = 'development.ini'
-setup_logging(ini_path)
-application = get_app(ini_path, 'main')
-serve(application, listen='*:6543')
+    ini_path = 'development.ini'
+    setup_logging(ini_path)
+    application = get_app(ini_path, 'main')
+    serve(application, listen='*:6543')
+
+if __name__ == "__main__":
+    main()
