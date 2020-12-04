54c8f7e41f0414d17c66fe2cb806f94070acf546
Peter Schaer <peter.schaer@be.ch>
2020-12-04 16:09:23 +0100

    setup.py erweitert. Version wird nun aus File ausgelesen.

--- setup.py
+++ setup.py
@@ -2,11 +2,26 @@
 from oereb_server import __version__
 import oereb_server
 import setuptools
+import os
+import codecs
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 setuptools.setup(
     name='oereb_server',
     packages=setuptools.find_packages(),
-    version=__version__,
+    version = get_version("oereb_server/__init__.py"),
     include_package_data=True,
     install_requires=['plaster_pastedeploy', 'pyramid', 'pyramid_jinja2', 'pyramid_debugtoolbar', 'waitress', 'pyramid_oereb[recommend]==1.8.0'],
     author='Peter Schär, Amt für Geoinformation des Kantons Bern',
